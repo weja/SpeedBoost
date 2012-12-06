fbfad658aea61805ac4f7aa5d27c671385dfb900
weja <weja_93@hotmail.com>
2012-12-06 22:56:36 +0200

    Update src/com/neverseenbefore/weja/speedboost/SpeedBoost.java
    Added strenghts for effects, I, II and III.
    Also added more permissions.

--- src/com/neverseenbefore/weja/speedboost/SpeedBoost.java
+++ src/com/neverseenbefore/weja/speedboost/SpeedBoost.java
@@ -15,31 +15,68 @@ public final class SpeedBoost extends JavaPlugin implements Listener {
     @Override
     public void onEnable(){
      getServer().getPluginManager().registerEvents(this, this);
+     getLogger().info("SpeedBoost v0.3 has been succesfully enabled!");
+    }
+    @Override
+    public void onDisable(){
+     getLogger().info("SpeedBoost v0.3 has been succesfully disabled! See ya!");
     }
     
     @EventHandler
     public void onJoin(PlayerJoinEvent event){
         Player p = event.getPlayer();
-        if(p.hasPermission("speedboost.speedboost")) {
+        if(p.hasPermission("speedboost.speedboost.one")) {
          p.removePotionEffect(PotionEffectType.SPEED);
+            p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 18000, 0));
+        }
+        if(p.hasPermission("speedboost.speedboost.two")) {
+        	p.removePotionEffect(PotionEffectType.SPEED);
             p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 18000, 1));
         }
-        if(p.hasPermission("speedboost.jump")) {
+        if(p.hasPermission("speedboost.speedboost.three")) {
+        	p.removePotionEffect(PotionEffectType.SPEED);
+            p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 18000, 2));
+        }
+        if(p.hasPermission("speedboost.jump.one")) {
+        	p.removePotionEffect(PotionEffectType.JUMP);
+            p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 18000, 0));
+        }
+        if(p.hasPermission("speedboost.jump.two")) {
         	p.removePotionEffect(PotionEffectType.JUMP);
             p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 18000, 1));
+        }
+        if(p.hasPermission("speedboost.jump.three")) {
+        	p.removePotionEffect(PotionEffectType.JUMP);
+            p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 18000, 2));
         }
     }
     
     @EventHandler
     public void onInventoryClose(InventoryCloseEvent event){
         Player p = (Player) event.getPlayer();
-        if(p.hasPermission("speedboost.speedboost")) {
+        if(p.hasPermission("speedboost.speedboost.one")) {
         	p.removePotionEffect(PotionEffectType.SPEED);
-        p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 18000, 1));
+            p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 18000, 0));
         }
-        if(p.hasPermission("speedboost.jump")) {
+        if(p.hasPermission("speedboost.speedboost.two")) {
+        	p.removePotionEffect(PotionEffectType.SPEED);
+            p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 18000, 1));
+        }
+        if(p.hasPermission("speedboost.speedboost.three")) {
+        	p.removePotionEffect(PotionEffectType.SPEED);
+            p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 18000, 2));
+        }
+        if(p.hasPermission("speedboost.jump.one")) {
         	p.removePotionEffect(PotionEffectType.JUMP);
-        p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 18000, 1));
+            p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 18000, 0));
+        }
+        if(p.hasPermission("speedboost.jump.two")) {
+        	p.removePotionEffect(PotionEffectType.JUMP);
+            p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 18000, 1));
+        }
+        if(p.hasPermission("speedboost.jump.three")) {
+        	p.removePotionEffect(PotionEffectType.JUMP);
+            p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 18000, 2));
         }
     }
 }
